720c2ae2986f9a8eb910a377e862e998a639ab4f
SagarMakhar <sagarmakhar@gmail.com>
2024-12-29 09:57:45 +0100

    SystemUI: Introduce dynamic VoLTE & VoWiFi icons From evolutionX devs
    @neobuddy89:
    * Clean up unrequired derps. Adapted for A14.
    * Disabled by default.
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I374d8630e766a9508df182974748441fe340e6e9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Adapted for A15 by dwardo
    
    Change-Id: Ic8c6cd4759d0e24260d9d217f7575e45368b1b90

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
@@ -29,6 +29,7 @@ import com.android.systemui.CoreStartable;
 import com.android.systemui.dagger.SysUISingleton;
 import com.android.systemui.res.R;
 import com.android.systemui.statusbar.connectivity.IconState;
+import com.android.systemui.statusbar.connectivity.ImsIconState;
 import com.android.systemui.statusbar.connectivity.NetworkController;
 import com.android.systemui.statusbar.connectivity.SignalCallback;
 import com.android.systemui.statusbar.phone.ui.StatusBarIconController;
@@ -61,6 +62,7 @@ public class StatusBarSignalPolicy
     private final String mSlotVpn;
     private final String mSlotNoCalling;
     private final String mSlotCallStrength;
+    private final String mSlotIms;
 
     private final Context mContext;
     private final StatusBarIconController mIconController;
@@ -76,6 +78,7 @@ public class StatusBarSignalPolicy
     private boolean mHideMobile;
     private boolean mHideEthernet;
     private final boolean mActivityEnabled;
+    private boolean mHideIms;
 
     private final ArrayList<CallIndicatorIconState> mCallIndicatorStates = new ArrayList<>();
     private boolean mInitialized;
@@ -109,6 +112,7 @@ public class StatusBarSignalPolicy
         mSlotCallStrength =
                 mContext.getString(com.android.internal.R.string.status_bar_call_strength);
         mActivityEnabled = mContext.getResources().getBoolean(R.bool.config_showActivity);
+        mSlotIms = mContext.getString(com.android.internal.R.string.status_bar_ims);
     }
 
     @Override
@@ -188,12 +192,14 @@ public class StatusBarSignalPolicy
         boolean hideAirplane = hideList.contains(mSlotAirplane);
         boolean hideMobile = hideList.contains(mSlotMobile);
         boolean hideEthernet = hideList.contains(mSlotEthernet);
+        boolean hideIms = hideList.contains(mSlotIms);
 
         if (hideAirplane != mHideAirplane || hideMobile != mHideMobile
-                || hideEthernet != mHideEthernet) {
+                || hideEthernet != mHideEthernet || hideIms != mHideIms) {
             mHideAirplane = hideAirplane;
             mHideMobile = hideMobile;
             mHideEthernet = hideEthernet;
+            mHideIms = hideIms;
             // Re-register to get new callbacks.
             mNetworkController.removeCallback(this);
             mNetworkController.addCallback(this);
@@ -285,6 +291,16 @@ public class StatusBarSignalPolicy
                     mSlotAirplane,
                     TelephonyIcons.FLIGHT_MODE_ICON,
                     mContext.getString(R.string.accessibility_airplane_mode));
+        }
+    }
+
+    @Override
+    public void setImsIcon(ImsIconState icon) {
+        if (icon.visible && !mHideIms) {
+            mIconController.setImsIcon(mSlotIms, icon);
+            mIconController.setIconVisibility(mSlotIms, true);
+        } else {
+            mIconController.setIconVisibility(mSlotIms, false);
         }
     }
 
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ui/IconManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ui/IconManager.java
@@ -18,6 +18,7 @@ package com.android.systemui.statusbar.phone.ui;
 
 import static com.android.systemui.statusbar.phone.StatusBarIconHolder.TYPE_BINDABLE;
 import static com.android.systemui.statusbar.phone.StatusBarIconHolder.TYPE_ICON;
+import static com.android.systemui.statusbar.phone.StatusBarIconHolder.TYPE_IMS;
 import static com.android.systemui.statusbar.phone.StatusBarIconHolder.TYPE_MOBILE_NEW;
 import static com.android.systemui.statusbar.phone.StatusBarIconHolder.TYPE_WIFI_NEW;
 
@@ -33,7 +34,9 @@ import com.android.systemui.demomode.DemoModeCommandReceiver;
 import com.android.systemui.modes.shared.ModesUiIcons;
 import com.android.systemui.statusbar.BaseStatusBarFrameLayout;
 import com.android.systemui.statusbar.StatusBarIconView;
+import com.android.systemui.statusbar.StatusBarImsView;
 import com.android.systemui.statusbar.StatusIconDisplayable;
+import com.android.systemui.statusbar.connectivity.ImsIconState;
 import com.android.systemui.statusbar.connectivity.ui.MobileContextProvider;
 import com.android.systemui.statusbar.phone.DemoStatusIcons;
 import com.android.systemui.statusbar.phone.StatusBarIconHolder;
@@ -151,6 +154,7 @@ public class IconManager implements DemoModeCommandReceiver {
             case TYPE_BINDABLE ->
                 // Safe cast, since only BindableIconHolders can set this tag on themselves
                 addBindableIcon((BindableIconHolder) holder, index);
+            case TYPE_IMS -> addImsIcon(index, slot, holder.getImsState());
             default -> null;
         };
     }
@@ -212,6 +216,13 @@ public class IconManager implements DemoModeCommandReceiver {
         return view;
     }
 
+    protected StatusBarImsView addImsIcon(int index, String slot, ImsIconState state) {
+        StatusBarImsView view = onCreateStatusBarImsView(slot);
+        view.applyImsState(state);
+        mGroup.addView(view, index, onCreateLayoutParams(Shape.WRAP_CONTENT));
+        return view;
+    }
+
     private StatusBarIconView onCreateStatusBarIconView(String slot, boolean blocked) {
         return new StatusBarIconView(mContext, slot, null, blocked);
     }
@@ -230,6 +241,11 @@ public class IconManager implements DemoModeCommandReceiver {
                         slot,
                         mMobileIconsViewModel.viewModelForSub(subId, mLocation)
                 );
+    }
+
+    private StatusBarImsView onCreateStatusBarImsView(String slot) {
+        StatusBarImsView view = StatusBarImsView.fromContext(mContext, slot);
+        return view;
     }
 
     protected LinearLayout.LayoutParams onCreateLayoutParams(Shape shape) {
@@ -280,8 +296,18 @@ public class IconManager implements DemoModeCommandReceiver {
             case TYPE_BINDABLE:
                 // Nothing, the new icons update themselves
                 return;
+            case TYPE_IMS:
+                onSetImsIcon(viewIndex, holder.getImsState());
+                return;
             default:
                 break;
+        }
+    }
+
+    public void onSetImsIcon(int viewIndex, ImsIconState state) {
+        StatusBarImsView view = (StatusBarImsView) mGroup.getChildAt(viewIndex);
+        if (view != null) {
+            view.applyImsState(state);
         }
     }
 
